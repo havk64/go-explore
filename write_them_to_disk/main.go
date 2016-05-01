2dd1ef2a77bd3c2959c74eaa645dd001b6094144
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-05-01 18:59:32 -0300

    Task 1 completed using ioutil.WriteFile

---
+++ write_them_to_disk/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	client := &http.Client{}
+
+	response, err := client.Get("https://api.github.com/search/repositories?q=language:go&sort=stars&order=desc")
+
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	}
+	defer response.Body.Close()
+	response.Header.Set("User-Agent", "Holberton_School")
+	response.Header.Set("Authorization", "token 6a54def2525aa32b003337b31487e321d6a2bb59")
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+	//fmt.Printf("%s", body)
+	error := ioutil.WriteFile("/tmpl/23", body, 0644)
+	if error != nil {
+		fmt.Fprintf(os.Stderr, "Something went wrong! \n%v", error)
+	}
+}
