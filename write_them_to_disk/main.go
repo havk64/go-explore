8b4ba998872f1483f2541d65765040c8dd8b70c2
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-05-01 20:23:41 -0300

    Using os.Create to write to file, handling error

--- write_them_to_disk/main.go
+++ write_them_to_disk/main.go
@@ -7,6 +7,12 @@ import (
 	"os"
 )
 
+func check(e error) {
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "Something went wrong!\n%v\n", e)
+	}
+}
+
 func main() {
 	client := &http.Client{}
 
@@ -20,11 +26,16 @@ func main() {
 	response.Header.Set("User-Agent", "Holberton_School")
 	response.Header.Set("Authorization", "token 6a54def2525aa32b003337b31487e321d6a2bb59")
 	body, err := ioutil.ReadAll(response.Body)
+	check(err)
+	file, err := os.Create("/tmp/23")
+	check(err)
+	file.Chmod(0666)
+	check(err)
+	defer file.Close()
+	w, err := file.Write(body)
 	if err != nil {
-		fmt.Printf("%s", err)
-	}
-	error := ioutil.WriteFile("/tmp/23", body, 0644)
-	if error != nil {
-		fmt.Fprintf(os.Stderr, "Something went wrong! \n%v", error)
+		fmt.Fprintf(os.Stderr, "Couldn't write the file becase of this error: %v\n", err)
+	} else {
+		fmt.Printf("The file was saved!\n%v bytes written", w)
 	}
 }
