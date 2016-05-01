44dddb4db973af9416db3441c5560c2b87b8d145
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-05-01 17:35:19 -0300

    Task 0 completed, Github Most Popular(without headers)

---
+++ github_most_popular/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	client := &http.Client{
+	//"User-Agent":  "Holberton_School",
+	//Authorization: "token 6a54def2525aa32b003337b31487e321d6a2bb59",
+	}
+
+	response, err := client.Get("https://api.github.com/search/repositories?q=language:go&sort=stars&order=desc")
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+	fmt.Printf("%s", body)
+}
