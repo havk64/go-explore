28f8f451a46de4c8434576222820325087eab756
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-05-01 17:57:18 -0300

    Adding headers to http request

--- github_most_popular/main.go
+++ github_most_popular/main.go
@@ -8,10 +8,7 @@ import (
 )
 
 func main() {
-	client := &http.Client{
-	//"User-Agent":  "Holberton_School",
-	//Authorization: "token 6a54def2525aa32b003337b31487e321d6a2bb59",
-	}
+	client := &http.Client{}
 
 	response, err := client.Get("https://api.github.com/search/repositories?q=language:go&sort=stars&order=desc")
 	if err != nil {
@@ -19,6 +16,8 @@ func main() {
 		os.Exit(1)
 	}
 	defer response.Body.Close()
+	response.Header.Set("User-Agent", "Holberton_School")
+	response.Header.Set("Authorization", "token 6a54def2525aa32b003337b31487e321d6a2bb59")
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		fmt.Printf("%s", err)
