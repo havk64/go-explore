89f0c454ff00723c7fd1be64a78ce0be09623213
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-05-01 22:01:54 -0300

    Deleting unnecessary comments, cleaning up

--- manipulate_the_data/main.go
+++ manipulate_the_data/main.go
@@ -21,24 +21,17 @@ func check(e error) {
 
 func main() {
 	client := &http.Client{}
-
 	response, err := client.Get("https://api.github.com/search/repositories?q=language:go&sort=stars&order=desc")
 	check(err)
-	//if err != nil {
-	//	fmt.Printf("%s", err)
-	//	os.Exit(1)
-	//}
 	defer response.Body.Close()
 	response.Header.Set("User-Agent", "Holberton_School")
 	response.Header.Set("Authorization", "token 6a54def2525aa32b003337b31487e321d6a2bb59")
-	//body, err := ioutil.ReadAll(response.Body)
-	check(err)
-	var github users //map[string]string
+	var github users
 	decoder := json.NewDecoder(response.Body)
 	error := decoder.Decode(&github)
 	check(error)
+	defer fmt.Println("Boooommm ! ! !")
 	for _, item := range github.Items {
 		fmt.Printf("%s\n", item.FullName)
 	}
-
 }
