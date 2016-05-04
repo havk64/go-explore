8bf98f376c74d50156d9b43ed9c5d1ba41a57538
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-05-04 12:35:34 -0300

    Fixing Header request using http client

--- github_most_popular/main.go
+++ github_most_popular/main.go
@@ -15,12 +15,16 @@ func check(e error) {
 
 func main() {
 	client := &http.Client{}
-	response, err := client.Get("https://api.github.com/search/repositories?q=language:go&sort=stars&order=desc")
+	url := "https://api.github.com/search/repositories?q=language:go&sort=stars&order=desc"
+	req, err := http.NewRequest("GET", url, nil)
 	check(err)
-	defer response.Body.Close()
-	response.Header.Set("User-Agent", "Holberton_School")
-	response.Header.Set("Authorization", "token 6a54def2525aa32b003337b31487e321d6a2bb59")
-	body, err := ioutil.ReadAll(response.Body)
+	req.Header.Set("User-Agent", "Holberton_School")
+	req.Header.Set("Authorization", "token 6a54def2525aa32b003337b31487e321d6a2bb59")
+	//At this point the request object is: *(1) (look below)
+	res, err := client.Do(req)
+	//At this point the response object is: *(2)
 	check(err)
-	fmt.Printf("%s", body)
+	data, err := ioutil.ReadAll(res.Body)
+	check(err)
+	fmt.Println(string(data))
 }
