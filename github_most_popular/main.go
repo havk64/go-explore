9413820bf48d1e06cea5613666143af7d1896cd0
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-05-04 11:26:48 -0300

    Adding function to deal with errors

--- github_most_popular/main.go
+++ github_most_popular/main.go
@@ -3,25 +3,24 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
-	"os"
 )
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
 
 func main() {
 	client := &http.Client{}
-
 	response, err := client.Get("https://api.github.com/search/repositories?q=language:go&sort=stars&order=desc")
-
-	if err != nil {
-		fmt.Printf("%s", err)
-		os.Exit(1)
-	}
+	check(err)
 	defer response.Body.Close()
 	response.Header.Set("User-Agent", "Holberton_School")
 	response.Header.Set("Authorization", "token 6a54def2525aa32b003337b31487e321d6a2bb59")
 	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		fmt.Printf("%s", err)
-	}
+	check(err)
 	fmt.Printf("%s", body)
 }
