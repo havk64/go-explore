393402d8b63c1aa9fb60364fb0e89eb05b9e7c14
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-05-05 02:35:46 -0300

    Fixing the req Header

--- write_them_to_disk/main.go
+++ write_them_to_disk/main.go
@@ -10,24 +10,19 @@ import (
 
 func check(e error) {
 	if e != nil {
-		//fmt.Fprintf(os.Stderr, "Something went wrong!\n%v\n", e)
 		log.Fatal(e)
 	}
 }
 
 func main() {
 	client := &http.Client{}
-
-	response, err := client.Get("https://api.github.com/search/repositories?q=language:go&sort=stars&order=desc")
-
-	if err != nil {
-		fmt.Printf("%s", err)
-		os.Exit(1)
-	}
-	defer response.Body.Close()
-	response.Header.Set("User-Agent", "Holberton_School")
-	response.Header.Set("Authorization", "token 6a54def2525aa32b003337b31487e321d6a2bb59")
-	body, err := ioutil.ReadAll(response.Body)
+	req, err := http.NewRequest("GET", "https://api.github.com/search/repositories?q=language:go&sort=stars&order=desc", nil) //Creating request.
+	req.Header.Set("User-Agent", "Holberton_School")
+	req.Header.Set("Authorization", "token 6a54def2525aa32b003337b31487e321d6a2bb59")
+	res, err := client.Do(req)
+	check(err)
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
 	check(err)
 	file, err := os.Create("/tmp/23")
 	check(err)
