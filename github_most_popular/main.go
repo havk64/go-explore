b13ad6746fa10b92fbcc6846f986e667675bdfbb
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-05-05 01:07:59 -0300

    Using functions of url.go, adding/improving comments

--- github_most_popular/main.go
+++ github_most_popular/main.go
@@ -14,12 +14,12 @@ func check(e error) {
 }
 
 func main() {
+	u := customURL() //Function created to split the url allowing easy customation.
 	client := &http.Client{}
-	url := "https://api.github.com/search/repositories?q=language:go&sort=stars&order=desc"
-	req, err := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest("GET", u.String(), nil)
 	check(err)
-	req.Header.Set("User-Agent", "Holberton_School")
-	req.Header.Set("Authorization", "token 6a54def2525aa32b003337b31487e321d6a2bb59")
+	req.Header = customHeader() //Function to split the header allowing easy customization.
+	fmt.Println(req.Header)
 	//At this point the request object is: *(1) (look below)
 	res, err := client.Do(req)
 	//At this point the response object is: *(2)
@@ -30,7 +30,7 @@ func main() {
 }
 
 /* ========================================= *
- * *(1) ==> Beginning of Request Object.     *
+ * *(1) ==> Beginning of Request Object. (1) *
  * ========================================= *
 &http.Request{
   Method: "GET",
@@ -71,7 +71,7 @@ func main() {
 } ==>> End of Request Object  <<<<<== */
 
 /* ========================================= *
- * *(2) ==> Beginning of Response Object.    *
+ * *(2) ==> Beginning of Response Object.(2) *
  * ========================================= *
 &http.Response{
   Status:     "200 OK",
