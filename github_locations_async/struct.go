3fa86ebd6f8336959f39642686f8b96e56bb2d5f
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-05-06 01:11:54 -0300

    Adding more comments

--- github_locations_async/struct.go
+++ github_locations_async/struct.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// Structure to fetch and parse the JSON data.
 type users struct {
 	Location string `json:"location"`
 	Items    []struct {
@@ -24,10 +25,14 @@ type HTTPResponse struct {
 	res   http.Response
 	//err   error
 }
+
+// userObject is the struct used as parameter to call the asyncHTTPGets
+// func and has the index to be used to sort the result in a proper way.
 type userObject struct {
 	index int
 	url   string
 	login string
 }
 
+// indexSorter is used to be able to sort the final result by the index.
 type indexSorter []*HTTPResponse
