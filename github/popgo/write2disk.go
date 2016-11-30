5cf8e4ede414b1dc94f35a16b225f48886374524
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-11-30 21:57:04 -0200

    Refactoring using more idiomatic error handling

--- github/popgo/write2disk.go
+++ github/popgo/write2disk.go
@@ -6,33 +6,51 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
-func check(e error) {
-	if e != nil {
-		log.Fatal(e)
+func main() {
+	start := time.Now()
+	body := request()
+
+	file, err := os.Create("/tmp/23")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer fmt.Printf("%v\n", time.Since(start))
+	defer file.Close()
+	w, err := file.Write(body)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing file: %v\n", err)
+	} else {
+		fmt.Printf("The file was saved!\n%v bytes written\n", w)
 	}
 }
 
-func main() {
+func request() []byte {
+	url := "https://api.github.com/search/repositories?q=language:go&sort=stars&order=desc"
 	client := &http.Client{}
-	req, err := http.NewRequest("GET", "https://api.github.com/search/repositories?q=language:go&sort=stars&order=desc", nil) //Creating request.
+
+	req, err := http.NewRequest("GET", url, nil) //Creating request.
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	req.Header.Set("User-Agent", "Holberton_School")
 	req.Header.Set("Authorization", "token 6a54def2525aa32b003337b31487e321d6a2bb59")
+
 	res, err := client.Do(req)
-	check(err)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
-	check(err)
-	file, err := os.Create("/tmp/23")
-	check(err)
-	file.Chmod(0666)
-	check(err)
-	defer file.Close()
-	w, err := file.Write(body)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Couldn't write the file becase of this error: %v\n", err)
-	} else {
-		fmt.Printf("The file was saved!\n%v bytes written", w)
+		log.Fatal(err)
 	}
+
+	return body
 }
