b948c777f7384815217a8c3afbf283d6f9197cf7
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-12-01 00:03:11 -0200

    Refactoring to use basic concurrency

--- github/popgo/manipulate.go
+++ github/popgo/manipulate.go
@@ -14,28 +14,45 @@ type users struct {
 	}
 }
 
-func check(e error) {
-	if e != nil {
-		log.Fatal(e)
+func main() {
+	start := time.Now()
+	response := <-request()
+
+	defer fmt.Println("Task executed in:", time.Since(start))
+	for _, item := range response.Items {
+		fmt.Printf("%s\n", item.FullName)
 	}
 }
 
-func main() {
-	start := time.Now()
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", "https://api.github.com/search/repositories?q=language:go&sort=stars&order=desc", nil) //Creating request.
-	req.Header.Set("User-Agent", "Holberton_School")
-	req.Header.Set("Authorization", "token 6a54def2525aa32b003337b31487e321d6a2bb59")
-	res, err := client.Do(req)
-	check(err)
-	var github users
-	defer res.Body.Close()
-	defer fmt.Println("Task executed in:", time.Since(start))
-	defer fmt.Println("BOOOOOMMMMM ! ! !")
-	decoder := json.NewDecoder(res.Body)
-	error := decoder.Decode(&github)
-	check(error)
-	for _, item := range github.Items {
-		fmt.Printf("%s\n", item.FullName)
-	}
+func request() <-chan users {
+	ch := make(chan users)
+	var gh users
+	go func() {
+		uri := "https://api.github.com/search/repositories?q=language:go&sort=stars&order=desc"
+		client := &http.Client{}
+
+		req, err := http.NewRequest("GET", uri, nil) //Creating request.
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		req.Header.Set("User-Agent", "Holberton_School")
+		req.Header.Set("Authorization", "token 6a54def2525aa32b003337b31487e321d6a2bb59")
+
+		res, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer res.Body.Close()
+		// body, err := ioutil.ReadAll(res.Body)
+		decoder := json.NewDecoder(res.Body)
+		err = decoder.Decode(&gh)
+		if err != nil {
+			log.Fatal(err)
+		}
+		ch <- gh
+		close(ch)
+	}()
+	return ch
 }
