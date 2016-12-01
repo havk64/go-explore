aa241a440125e01139aa820658f95ed52103ddee
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-11-30 22:26:56 -0200

    Implementing basic concurrency using goroutine/channel on http req

--- github/popgo/write2disk.go
+++ github/popgo/write2disk.go
@@ -20,7 +20,7 @@ func main() {
 
 	defer fmt.Printf("%v\n", time.Since(start))
 	defer file.Close()
-	w, err := file.Write(body)
+	w, err := file.Write(<-body)
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error writing file: %v\n", err)
@@ -29,28 +29,33 @@ func main() {
 	}
 }
 
-func request() []byte {
-	url := "https://api.github.com/search/repositories?q=language:go&sort=stars&order=desc"
-	client := &http.Client{}
+func request() <-chan []byte {
+	ch := make(chan []byte)
+	go func() {
+		url := "https://api.github.com/search/repositories?q=language:go&sort=stars&order=desc"
+		client := &http.Client{}
 
-	req, err := http.NewRequest("GET", url, nil) //Creating request.
-	if err != nil {
-		log.Fatal(err)
-	}
+		req, err := http.NewRequest("GET", url, nil) //Creating request.
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	req.Header.Set("User-Agent", "Holberton_School")
-	req.Header.Set("Authorization", "token 6a54def2525aa32b003337b31487e321d6a2bb59")
+		req.Header.Set("User-Agent", "Holberton_School")
+		req.Header.Set("Authorization", "token 6a54def2525aa32b003337b31487e321d6a2bb59")
 
-	res, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
+		res, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	defer res.Body.Close()
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+		defer res.Body.Close()
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	return body
+		ch <- body
+	}()
+
+	return ch
 }
