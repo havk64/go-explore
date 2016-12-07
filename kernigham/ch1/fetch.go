34641feca3d8283889621a446dd194f06fa394b3
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-12-06 23:00:43 -0200

    Add client to fetch and test server-lissajous exercise

---
+++ kernigham/ch1/fetch.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+func main() {
+	client := &http.Client{}
+	uri, err := url.Parse("http://localhost:8000/lissajous")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error parsing: %v\n", err)
+	}
+
+	body := &url.Values{
+		"cycles": {"20"},
+	}
+
+	uri.RawQuery = body.Encode()
+	req, err := http.NewRequest("GET", uri.String(), nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Request error: %v\n", err)
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Request error: %v\n", err)
+		os.Exit(1)
+	}
+
+	n, err := io.Copy(os.Stdout, res.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error copying response body: %v\n", err)
+	}
+
+	fmt.Printf("%#v\n", n)
+	defer res.Body.Close()
+}
