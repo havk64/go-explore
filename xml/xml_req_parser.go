3eb01d483efc58ab8e28d8e7c728cf42cb1925c0
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-12-10 14:42:38 -0200

    Add comments

--- xml/xml_req_parser.go
+++ xml/xml_req_parser.go
@@ -11,31 +11,38 @@ import (
 )
 
 func main() {
+	// Create the http client
 	client := &http.Client{}
 
+	// Build the query string
 	body := &url.Values{
 		"key":  {"QaeIyNYQ484uU9WG3XxNw"},
 		"id":   {"4"},
 		"page": {"1"},
 	}
 
+	// Parse the Url
 	uri, err := url.Parse("https://www.goodreads.com/author/list.xml")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "request error: %v", err)
 	}
 
+	// Put the encoded string in the url
 	uri.RawQuery = body.Encode()
 
+	// Build the http request
 	req, err := http.NewRequest("GET", uri.String(), nil)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "request error: %v", err)
 	}
 
+	// Send the http request and get the result
 	res, err := client.Do(req)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "request error: %v", err)
 	}
 
+	// Parse the result
 	xd := xml.NewDecoder(res.Body)
 
 	for {
@@ -48,5 +55,6 @@ func main() {
 		}
 		fmt.Printf("%s\n", tok)
 	}
+	//Close the response body
 	res.Body.Close()
 }
