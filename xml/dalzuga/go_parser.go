41ffff14e33cb01d27c580719e6069377f478637
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-12-12 18:11:06 -0200

    Clean up commented pieces of code

--- xml/dalzuga/go_parser.go
+++ xml/dalzuga/go_parser.go
@@ -23,8 +23,6 @@ func main() {
 
 	AuthorID := grbq.Book.Authors[0].ID
 
-	// fmt.Println("AuthorID:", AuthorID)
-
 	fileBytes, err = ioutil.ReadFile("authorlistbooks.xml")
 	if err != nil {
 		log.Fatal(err)
@@ -79,24 +77,13 @@ func makeHTTPRequest(uri string, AuthorID int, pageNumber int, graq *GoodReadsAu
 	if err != nil {
 		log.Fatal(err)
 	}
-	// fmt.Println("Host:", u.Host)
-	// u.Scheme = "https"
-	// u.Host = "goodreads.com"
 
 	q := u.Query()
 	q.Set("key", `kDkKnUxiz8cRBJhVjrtSA`)
 	q.Set("id", strconv.Itoa(AuthorID))
 	q.Set("page", strconv.Itoa(pageNumber))
-
-	// fmt.Println(q.Encode())
-
 	u.RawQuery = q.Encode()
-
-	// fmt.Println(u.Host)
-	// fmt.Println(u.RequestURI())
-
 	fullURL := u.String()
-	// fmt.Println(fullURL)
 
 	req, err := http.NewRequest("GET", fullURL, nil)
 	if err != nil {
@@ -108,14 +95,6 @@ func makeHTTPRequest(uri string, AuthorID int, pageNumber int, graq *GoodReadsAu
 		log.Fatal(err)
 	}
 
-	/* Uncomment lines below to dump the http response */
-	// dump, err := httputil.DumpResponse(resp, true)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	//
-	// fmt.Printf("%q", dump)
-
 	requestBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
@@ -125,6 +104,4 @@ func makeHTTPRequest(uri string, AuthorID int, pageNumber int, graq *GoodReadsAu
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// fmt.Printf("%#v", graq.Author.Books.Book[0].Title)
 }
