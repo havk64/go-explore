7df8802238e50dc3cb4439c7c8b70f044af87349
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2016-12-15 19:39:58 -0200

    Add unit test to test scrape function

--- xml/advanced_parser/example_test.go
+++ xml/advanced_parser/example_test.go
@@ -1,6 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestID(t *testing.T) {
+	var expected = map[string]interface{}{
+		"title": "3",
+	}
+
+	selectors := map[string]interface{}{
+		"title": "GoodreadsResponse book authors author id",
+	}
+
+	elements := scrape("books.xml", selectors)
+	eq := reflect.DeepEqual(elements, expected)
+	if !eq {
+		t.Errorf("Expected: %v, got: %v", expected, elements) //("Scrape(%s) = %v, want %v", elements, expected)
+	}
+}
 
 func Example() {
 	selectors := map[string]interface{}{
@@ -10,4 +30,5 @@ func Example() {
 	fmt.Printf("%v\n", elements)
 	// Output:
 	// map[title:4]
+
 }
